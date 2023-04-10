409a140fb3ea534cbb965bf2a111608512e07ff8
john cattanach <john.t.cattanach@gmail.com>
2023-04-10 08:43:04 +0100

    removed file reference

--- map-bag.py
+++ map-bag.py
@@ -8,15 +8,14 @@ def validword(s):
     else:
         return s.lower()
 # imitate stdin
-with open('twitter_MBTI.csv') as f:
-    df = pd.read_csv(sys.stdin)
-    # Iterate over each row in the csv
-    # file using reader object
-    #df.info()
-    for i,row in df.iterrows():
-        for tweet in row['text'].split('|||'):
-            for word in tweet.split():
-                # ignore urls and handles
-                w=validword(word)
-                if w != '':
-                    print(f'{w}\t1')
+df = pd.read_csv(sys.stdin)
+ # Iterate over each row in the csv
+ # file using reader object
+ #df.info()
+for i,row in df.iterrows():
+    for tweet in row['text'].split('|||'):
+        for word in tweet.split():
+            # ignore urls and handles
+            w=validword(word)
+            if w != '':
+                print(f'{w}\t1')
