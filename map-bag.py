46769c96b8c7c41fab0b1ea5ac1832e4a4b6e025
JOhn cATTANACH <john.t.cattanach@gmail.com>
2023-04-10 10:44:15 +0100

    checkpoint on prelimary update to doc with map reduce

--- map-bag.py
+++ map-bag.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python
+# map-bag.py
+# map incoming csv into word,mbti type tuples
+# nb using pandas dataframe for prototyping purposes
 import sys
 import re
 import pandas as pd
-def validword(s):
-    if re.match(r'^http|^@',s):
-        return 'x'
+def validword(s):    
+    if s.isascii():  # ignore emtocicon and non-english text
+        if re.match(r'^http|^@',s):
+            return ''
+        else:
+            return s.lower()
     else:
-        return s.lower()
-# imitate stdin
-with open('twitter_MBTI.csv') as f:
-    df = pd.read_csv(sys.stdin)
-    # Iterate over each row in the csv
-    # file using reader object
-    #df.info()
-    for i,row in df.iterrows():
-        for tweet in row['text'].split('|||'):
-            for word in tweet.split():
-                # ignore urls and handles
-                w=validword(word)
-                if w != '':
-                    print(f'{w}\t1')
+        return ''
+# replace stdin for development
+df = pd.read_csv(sys.stdin)
+# Iterate over each row in the csv
+# file using reader object
+# NB in production would replace with a streaming equivalent
+for i,row in df.iterrows():
+    for tweet in row['text'].split('|||'):
+        for word in tweet.split():
+            # ignore urls and handles
+            w=validword(word)
+            if w != '':
+                print(f'{w}\t{row["label"]}\t1')
